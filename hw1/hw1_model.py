44242e3ac7c065cc6e15eaa49fab65044fc1a4e9
leadtheway <773740603@qq.com>
2020-03-17 18:44:06 -0700

    git commit -m

--- hw1/hw1_model.py
+++ hw1/hw1_model.py
@@ -1,2 +1,8 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+
+train_data = pd.read_csv('hw1/train.csv','r',encoding='Big5',index_col=False,delimiter=',')
+print(train_data)
+rows = np.size(train_data,0)
+Pm = []
